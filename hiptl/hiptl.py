80f39da64dac6b949ffc9edd0434358101124c07
calvinosinga <calvinosinga@gmail.com>
2020-12-31 16:52:16 -0500

    added some slurm submit files and going to try a test run soon

---
+++ hiptl/hiptl.py
@@ -0,0 +1,34 @@
+import numpy as np
+import h5py as hp
+import sys
+import zipfile as zf
+import os
+CHUNK = sys.argv[1]
+BASE = '/lustre/diemer/illustris/hih2/' 
+HOME = '/lustre/cosinga/ptl99/'
+SAVE = '/lustre/cosinga/hiptl_output/'
+BOXSIZE = 75000 #kpc/h
+grid = (2048,2048,2048)
+fileno = 448
+field = np.zeros(grid, dtype=np.float32) # 35 GB
+edges = np.linspace(0,BOXSIZE,grid[0]-1)
+hih2file = hp.File(BASE+"hih2_particles_099."+CHUNK+".hdf5", 'r')
+ptlfile = hp.File(HOME+"snap_099."+CHUNK+".hdf5", 'r')
+mass = ptlfile['PartType0']['Masses'][:] #~50 MB
+pos = ptlfile['PartType0']['CenterOfMass'][:] # ~150 MB
+f_neut_h = hih2file['PartType0']['f_neutral_H'][:] #~100 MB
+models = ['GD14', 'GK11', 'K13', 'S14']
+#models = ['GD14']
+w = hp.File(SAVE+'hiptl_99.'+CHUNK+'.hdf5', 'w')
+bins = np.digitize(pos, edges) # ~300 MB
+for m in models:
+    h2_frac = hih2file['PartType0']['f_mol_'+m][:] # ~100 MB
+    masshi = (1-h2_frac)*f_neut_h*mass # ~100 MB
+    masshi = masshi.astype('float32')# decreases size by 50 MB
+    for ptl,b in enumerate(bins):
+        field[b[0],b[1],b[2]] += masshi[ptl]
+    w.create_dataset(m, data=field)
+wzip = zf.ZipFile(SAVE+'hiptl_99.'+CHUNK+'.hdf5.zip', 'w')
+wzip.write(SAVE+'hiptl_99.'+CHUNK+'.hdf5', compress_type=zf.ZIP_DEFLATED)
+wzip.close()
+os.remove(SAVE+'hiptl_99.'+CHUNK+'.hdf5')
---
+++ hisubhalo/swanson.py
@@ -0,0 +1,60 @@
+import numpy as np
+import h5py as hp
+import sys
+import zipfile as zf
+import os
+
+########## INPUTS #################
+grid = (2048,2048,2048)
+CHUNK = sys.argv[1]
+LUM_MIN = -16 #detection minimum from Swanson
+SNAPSHOT = sys.argv[2]
+BOXSIZE = 75000 #kpc/h
+HOME = '/lustre/cosinga/subhalo'+str(SNAPSHOT)+'/'
+SAVE = '/lustre/cosinga/subhalo_output/'
+def isred(gr, rband):#color definition as given in Swanson
+    return gr> .9 - .03*(rband+23)
+###################################
+logfile = open(SAVE+'swanson_log'+str(SNAPSHOT)+'.txt', 'a')
+try:
+    f = hp.File(HOME+'fof_subhalo_tab_0'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5','r')
+except IOError:
+    logfile.write('failed to open file for '+str(CHUNK)+'\n')
+else:
+    try:
+        pos = f['Subhalo']['SubhaloCM'] #kpc/h
+        mass = f['Subhalo']['SubhaloMass']
+        photo = f['Subhalo']['SubhaloStellarPhotometrics']
+    except:
+        logfile.write("chunk "+str(CHUNK)+ '\'s subhalo data was empty')
+    else:
+        redfield = np.zeros(grid, dtype=np.float32)
+        bluefield = np.zeros(grid, dtype=np.float32)
+        nondetfield = np.zeros(grid, dtype=np.float32)
+        counts = np.zeros(3)
+        edges = np.linspace(0,BOXSIZE, grid[0]-1) #definitions of bins
+        bins = np.digitize(pos,edges)
+        for j,b in enumerate(bins):
+            rmag = photo[j][5]
+            gmag = photo[j][4]
+            if rmag<=LUM_MIN and isred(gmag-rmag,rmag):
+                redfield[b[0],b[1],b[2]]+= mass[j]
+                counts[2]+=1
+            if rmag<=LUM_MIN and not isred(gmag-rmag,rmag):
+                bluefield[b[0],b[1],b[2]]+= mass[j]
+                counts[0]+=1
+            if not rmag<=LUM_MIN:
+                nondetfield[b[0],b[1],b[2]]+= mass[j]
+                counts[1]+=1
+        w = hp.File(SAVE+'swanson_'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5', 'w')
+        w.create_dataset("red",data=redfield)
+        w.create_dataset("blue",data=bluefield)
+        w.create_dataset("nondetection",data=nondetfield)
+        w.create_dataset('counts',data=counts)
+        w.close()
+        wzip = zf.ZipFile(SAVE+'swanson_'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5.zip', 'w')
+        wzip.write(SAVE+'swanson_'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5', compress_type=zf.ZIP_DEFLATED)
+        wzip.close()
+        os.remove(SAVE+'swanson_'+str(SNAPSHOT)+'.'+str(CHUNK)+'.hdf5')
+
+
