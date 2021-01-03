4fceee7ac5e892747b5d2c349113d788500b7b44
root <root@DESKTOP-KKAMGI0.localdomain>
2021-01-03 16:01:25 -0500

    adjusting the combine procedure to be a bit more memory efficient

--- combine/hiptl_combine.py
+++ combine/hiptl_combine.py
@@ -6,35 +6,34 @@ import os
 BASE = '/lustre/cosinga/hiptl_output/'
 START = sys.argv[1]
 END = sys.argv[2]
-files = np.arange(int(START), int(END))
+filenos = np.arange(int(START), int(END))
 SNAPSHOT = sys.argv[3]
 STATUS = sys.argv[4]
-total = np.zeros((4, 2048, 2048, 2048), dtype=np.float32)
+
 logfile = open(BASE+"combine_test_log.txt",'w')
-for i in files:
-    #logfile.write("unzipping file " + str(i)+"\n")
-    #rzip = zf.ZipFile(BASE + "hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5.zip','r')
-    #rzip.extractall()
-    #rzip.close()
-    #logfile.write("unzipped file " + str(i)+"\n")
-    logfile.write("at this point, the running total has size: " + str(sys.getsizeof(total))+"\n")
-    logfile.write("now starting to add to running total:\n")
-    if STATUS == 'delete':
-        os.remove(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5.zip')
-    f = hp.File(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5','r')
-    logfile.write("the hdf5 file that got opened has size " + str(sys.getsizeof(f))+"\n")
-    keys = list(f.keys())
-    logfile.write("size of the array being added: "+ str(sys.getsizeof(f[keys[0]][:]))+"\n")
-    for k in range(keys):
-        total[k,:,:,:] += f[keys[k]][:]
-    logfile.write("after adding, the running total has size: " + str(sys.getsizeof(total))+"\n")
-    f.close()
 w = hp.File(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5','w')
-for k in range(keys):
-    w.create_dataset(keys[k], data=total[k,:,:,:])
-w.close()
+models = ['GD14', 'GK11', 'K13', 'S14']
+files = ['hiptl_'+str(SNAPSHOT)+'.'+str(i)+'.hdf5' for i in filenos]
+for m in models:
+    total = np.zeros((2048, 2048, 2048), dtype=np.float32)
+    logfile.write("starting model "+m+'\n')
+    for i in files:
+        logfile.write("at this point, the running total has size: " + str(sys.getsizeof(total))+"\n")
+        logfile.write("now starting to add to running total:\n")
+        f = hp.File(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5','r')
+        if STATUS == 'delete':
+            os.remove(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5.zip')
+        logfile.write("the hdf5 file that got opened has size " + str(sys.getsizeof(f))+"\n")
+        logfile.write("size of the array being added: "+ str(sys.getsizeof(f[keys[0]][:]))+"\n")
+        total += f[m][:]
+        logfile.write("after adding, the running total has size: " + str(sys.getsizeof(total))+"\n")
+        f.close()
+    w.create_dataset(m, data=total)
+
 wzip = zf.ZipFile(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5.zip', 'w')
 wzip.write(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5', compress_type=zf.ZIP_DEFLATED)
 wzip.close()
 if STATUS=='delete':
     os.remove(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5')
+logfile.close()
+w.close()
