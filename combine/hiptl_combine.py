09a4832f84f01352a914ded8a9e2bcc1b682fd2b
Calvin Osinga <cosinga@login-2.deepthought2.umd.edu>
2021-01-03 20:30:31 -0500

    fixed some bugs for second run attempt of combine

--- combine/hiptl_combine.py
+++ combine/hiptl_combine.py
@@ -20,20 +20,19 @@ for m in models:
     for i in files:
         logfile.write("at this point, the running total has size: " + str(sys.getsizeof(total))+"\n")
         logfile.write("now starting to add to running total:\n")
-        f = hp.File(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5','r')
-        if STATUS == 'delete':
-            os.remove(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5.zip')
+        f = hp.File(BASE+str(i),'r')
+#        if STATUS == 'delete':
+#            os.remove(BASE+"hiptl_"+str(SNAPSHOT)+'.'+str(i)+'.hdf5.zip')
         logfile.write("the hdf5 file that got opened has size " + str(sys.getsizeof(f))+"\n")
-        logfile.write("size of the array being added: "+ str(sys.getsizeof(f[keys[0]][:]))+"\n")
+        logfile.write("size of the array being added: "+ str(sys.getsizeof(f[m][:]))+"\n")
         total += f[m][:]
         logfile.write("after adding, the running total has size: " + str(sys.getsizeof(total))+"\n")
         f.close()
     w.create_dataset(m, data=total)
-
+w.close()
 wzip = zf.ZipFile(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5.zip', 'w')
 wzip.write(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5', compress_type=zf.ZIP_DEFLATED)
 wzip.close()
 if STATUS=='delete':
     os.remove(BASE+"hiptl_"+SNAPSHOT+'.'+START+'.'+END+'.hdf5')
 logfile.close()
-w.close()
