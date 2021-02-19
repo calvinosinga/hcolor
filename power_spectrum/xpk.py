480cfafde29ebd15f1992a5e596d9841456393c5
root <root@DESKTOP-KKAMGI0.localdomain>
2021-02-18 19:44:44 -0500

    making new xpk runs

--- power_spectrum/xpk.py
+++ power_spectrum/xpk.py
@@ -9,7 +9,7 @@ firstfield = sys.argv[2]
 secondfile = sys.argv[3]
 secondfield = sys.argv[4]
 savename = sys.argv[5]
-BOXSIZE = 75000.0 #kpc/h
+BOXSIZE = 75.0 #Mpc/h
 grid = (2048, 2048, 2048)
 print('opening files')
 f = hp.File(HOME+firstfile, 'r')
@@ -32,6 +32,6 @@ for i in ffields:
             fgrid = fgrid/np.mean(fgrid); fgrid = fgrid - 1
             ggrid = ggrid/np.mean(ggrid); ggrid = ggrid - 1
             print('turned fields into overdensities')
-            res = XPk([fgrid, ggrid], BOXSIZE, axis = 0, MAS=['NGP', 'NGP'])
+            res = XPk([fgrid, ggrid], BOXSIZE, axis = 0, MAS=['CIC', 'CIC'])
             xpk = np.transpose([res.k3D, res.XPk[:,0,0]])
             np.savetxt(HOME+'pk/'+savename+'_'+i+'-'+j+'.txt', xpk)
