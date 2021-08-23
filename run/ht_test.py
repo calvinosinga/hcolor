c0b23a7d7a4422bb580940abf16d25329f7f7b95
calvinosinga <calvinosinga@gmail.com>
2021-08-23 14:08:12 -0400

    testing the hydrotools run

---
+++ run/ht_test.py
@@ -0,0 +1,12 @@
+
+from hydrotools.interface import interface as iface_run
+
+
+xtf = ['gr_dust']
+shf = ['gr_normal', 'SubhaloPos', 'SubhaloVel', 'SubhaloMassType']
+# fields from all of the particles
+ptf = ['Coordinates','Velocities', 'Masses']
+iface_run.extractGalaxyData(sim='tng75', snap_idx=99, paranoid=True, output_path='/lustre/cosinga/HI-color/results/', 
+            file_suffix='_all_test', output_compression='gzip',catxt_get=True, catxt_fields=xtf, catsh_get=True, catsh_fields=shf,
+            ptlstr_get=True, ptlstr_fields=ptf)
+
