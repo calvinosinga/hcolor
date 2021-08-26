b03b46f49197ea41e51aa303c08d4b87e26e0263
Calvin Osinga <cosinga@login-3.deepthought2.umd.edu>
2021-08-26 09:28:57 -0400

    new ht_test

--- run/ht_test.py
+++ run/ht_test.py
@@ -8,5 +8,5 @@ shf = ['gr_normal', 'SubhaloPos', 'SubhaloVel', 'SubhaloMassType']
 ptf = ['Coordinates','Velocities', 'Masses']
 iface_run.extractGalaxyData(sim='tng75', snap_idx=99, paranoid=True, output_path='/lustre/cosinga/HI-color/results/', 
             file_suffix='_all_test', output_compression='gzip',catxt_get=True, catxt_fields=xtf, catsh_get=True, catsh_fields=shf,
-            ptlstr_get=True, ptlstr_fields=ptf, num_processes = 10, randomize_order=True, verbose=True)
+            ptlstr_get=True, ptlstr_fields=ptf, num_processes = 20, randomize_order=True)
 
