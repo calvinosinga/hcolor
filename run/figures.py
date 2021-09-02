47666bb3af078986c8bb1560f2da5ecc5e015abd
Calvin Osinga <cosinga@apophis.astro.umd.edu>
2021-09-02 09:52:51 -0400

    building figures

--- run/figures.py
+++ run/figures.py
@@ -5,21 +5,17 @@ import matplotlib.gridspec as gspec
 import sys
 import os
 
-GDPATH = sys.argv[1]
-PLOTDIR = sys.argv[2]
 
-gd = pkl.load(open(GDPATH, 'rb'))
-
-def HI_auto_pk():
+def HI_auto_pk(pkl_files, outdir):
     """
     Makes two HI-auto power spectra plots, for real space and redshift space.
 
     Dependencies: hiptl, hisubhalo, vn
     """
-
+    
     return
 
-def HI_cross_pk_methodology():
+def HI_galaxy_Xpk_methodology():
     """
     HI-galaxy cross powers separated into different panels by their methodologies.
     This plot is designed to go into the results section of the paper.
@@ -28,9 +24,10 @@ def HI_cross_pk_methodology():
     """
     return
 
-def HI_cross_pk_color():
+def HI_galaxy_Xpk_color():
     """
     HI-galaxy cross powers separated into different panels by their colors.
+    Made for both redshift-space and real-space.
     This plot is designed to go into the methods section of the paper.
 
     Dependencies: hiptl, hisubhalo, vn, galaxy
@@ -40,6 +37,9 @@ def HI_cross_pk_color():
 def color_def_sensitivity():
     return
 
+def HI_ptl_Xpk():
+    return
+
 def dust_sensitivity():
     return
 
@@ -47,4 +47,16 @@ def gr_stmass():
     return
 
 def galaxy_auto_pk():
+    return
+
+def stmass_vs_all_mass():
+    return
+
+def hiptl_nh_pk():
+    return
+
+def grid_resolution_test():
+    return
+
+def sim_resolution_test():
     return
