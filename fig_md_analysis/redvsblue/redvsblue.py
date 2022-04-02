4c4e2d48ce2a4291bb9fef36bb2a74eadbd3a7c2
calvinosinga <calvinosinga@gmail.com>
2022-04-02 13:49:24 -0400

    png

--- fig_md_analysis/redvsblue/redvsblue.py
+++ fig_md_analysis/redvsblue/redvsblue.py
@@ -124,26 +124,34 @@ def color_compare(ip, smooth, savename):
     return
 
 def smooth_compare(smooth_vals):
-    # def _smooth(data):
-    #     x = []
-    #     ymin = []
-    #     ymax = []
+    def _smooth(ax, data, smooth):
+        x = []
+        ymin = []
+        ymax = []
 
-    #     idx = 0
-    #     while idx < len(data[0]):
-    #         x.append(data[0][idx])
-    #         ymin.append(np.mean(data[1][idx:idx+smooth]))
-    #         ymax.append(np.mean(data[2][idx:idx+smooth]))
-    #         idx += smooth
+        idx = 0
+        while idx < len(data[0]):
+            x.append(data[0][idx])
+            ymin.append(np.mean(data[1][idx:idx+smooth]))
+            ymax.append(np.mean(data[2][idx:idx+smooth]))
+            idx += smooth
 
 
-    #     ax.fill_between(x, ymin, ymax, **kwargs)      
-    #     return
+        ax.fill_between(x, ymin, ymax, label = 'smooth%d'%smooth)      
+        return
+    
     ip = {'snapshot':99, 'vn_fieldname':'vn'}
     dl = DataList(master.getMatching(ip))
     rob = flib.makeBlueRedRatio(dl)
-    print('smooth compare')
-    print(len(rob))
+    fig, axs = plt.subplots(2, 1)
+    for sm in smooth_vals:
+        for i in [0, 1]:
+            p = axs[i]
+            data = rob[i].data
+            _smooth(p, data, sm)
+    
+    fig.savefig('smooth_compare.png')
+
     return
 
 ip = {'color':['red', 'blue']}
@@ -157,65 +165,67 @@ for smooth_val in smooth_vals:
 
 smooth_compare(smooth_vals)
 
-rbonly = DataList(master.getMatching({'color':['red', 'blue']}))
-withrat = flib.makeBlueRedRatio(rbonly)
-rbonly.dclist.extend(withrat)
+def redshift_evo():
 
-fg = Figrid(rbonly)
-fg.setRowOrder(['real', 'redshift'])
-fg.setColOrder(['blue', 'red', 'ratio'])
-fg.arrange('color', 'space', panel_length = 2, panel_bt = 0.11, yborder = 0.3)
-zcols = sb.color_palette('summer', len(master.getAttrVals('snapshot')))
-pargs = {}
-pargs['label'] = 'z=0'
-pargs['color'] = zcols[0]
-pargs['alpha'] = 0.55
-fg.makeFills({'snapshot': 99}, pargs)
-pargs['label'] = 'z=0.5'
-pargs['color'] = zcols[1]
-fg.makeFills({'snapshot':67}, pargs)
-fg.plot()
+    rbonly = DataList(master.getMatching({'color':['red', 'blue']}))
+    withrat = flib.makeBlueRedRatio(rbonly)
+    rbonly.dclist.extend(withrat)
 
-pkslc = (slice(0,2), slice(None))
-# fix the axes
-axparams = {}
-flib.setNyq(fg, kmin, res, box)
-axparams['xscale'] = 'log'
-axparams['ylim'] = [0, 2]
-fg.setAxisParams(axparams)
-axparams['yscale'] = 'log'
-axparams['ylim'] = [0.1, 1e4]
-fg.setAxisParams(axparams, slc=pkslc)
-# axparams['ylim'] = [1, 1e4]
-# fg.setAxisParams(axparams, slc=(slice(0,1), slice(0,2)))
-# fix the tick labels
-fg.setDefaultTicksParams()
-fg.setTicks({'labelsize':smfont, 'direction':'in'})
-# labels
-kw = {'fontsize':larfont}
-ypos = [0, 1 - 0.5*(fg.panel_length * 2 + fg.panel_bt[1] * 1 + fg.yborder[1]) / fg.figsize[1]]
-flib.pklabels(fg, ysub = r'\rm{HI-gal}', ypos = ypos, xtxtkw = kw, ytxtkw = kw)
-txtkw = {}
-txtkw['ha'] = 'center'
-txtkw['va'] = 'top'
-txtkw['fontsize'] = smfont
-fg.setRowLabels(['Blue Galaxies', 'Red Galaxies', 'Color Ratio'], [0.5, 0.95],
-               txtkw)
-txtkw['ha'] = 'left'
-txtkw['va'] = 'bottom'
-fg.setColLabels(['Real Space', 'Redshift Space'], [0.05, 0.05], txtkw)
-fg.makeYLabel(r'P$_{\rm{red}}$ (k) / P$_{\rm{blue}}$ (k)', 
-              [0, (0.5 * fg.panel_length + fg.yborder[0]) / fg.figsize[1]], 
-              {'va':'center', 'fontsize':larfont})
-lkw = {'frameon':False, 'fontsize':smfont - 1, 'loc':'lower left'}
-fg.drawLegend(lkw, (1,1))
-lkw['loc'] = 'center right'
-# fg.drawLegend(lkw, (2,0))
-fcolors = np.empty(fg.dim, dtype = object)
-trgba = mpl.colors.to_rgba
-alpha = 0.15
-for i in range(2):
-    flib.plotOnes(fg, (2, i))
-# fcolors[:,0] = [trgba('blue', alpha), trgba('red', alpha), trgba('white')]
-# flib.setFacecolor(fg, fcolors)
-fg.save("redvsblue_redshift_evo.pdf")
+    fg = Figrid(rbonly)
+    fg.setRowOrder(['real', 'redshift'])
+    fg.setColOrder(['blue', 'red', 'ratio'])
+    fg.arrange('color', 'space', panel_length = 2, panel_bt = 0.11, yborder = 0.3)
+    zcols = sb.color_palette('summer', len(master.getAttrVals('snapshot')))
+    pargs = {}
+    pargs['label'] = 'z=0'
+    pargs['color'] = zcols[0]
+    pargs['alpha'] = 0.55
+    fg.makeFills({'snapshot': 99}, pargs)
+    pargs['label'] = 'z=0.5'
+    pargs['color'] = zcols[1]
+    fg.makeFills({'snapshot':67}, pargs)
+    fg.plot()
+
+    pkslc = (slice(0,2), slice(None))
+    # fix the axes
+    axparams = {}
+    flib.setNyq(fg, kmin, res, box)
+    axparams['xscale'] = 'log'
+    axparams['ylim'] = [0, 2]
+    fg.setAxisParams(axparams)
+    axparams['yscale'] = 'log'
+    axparams['ylim'] = [0.1, 1e4]
+    fg.setAxisParams(axparams, slc=pkslc)
+    # axparams['ylim'] = [1, 1e4]
+    # fg.setAxisParams(axparams, slc=(slice(0,1), slice(0,2)))
+    # fix the tick labels
+    fg.setDefaultTicksParams()
+    fg.setTicks({'labelsize':smfont, 'direction':'in'})
+    # labels
+    kw = {'fontsize':larfont}
+    ypos = [0, 1 - 0.5*(fg.panel_length * 2 + fg.panel_bt[1] * 1 + fg.yborder[1]) / fg.figsize[1]]
+    flib.pklabels(fg, ysub = r'\rm{HI-gal}', ypos = ypos, xtxtkw = kw, ytxtkw = kw)
+    txtkw = {}
+    txtkw['ha'] = 'center'
+    txtkw['va'] = 'top'
+    txtkw['fontsize'] = smfont
+    fg.setRowLabels(['Blue Galaxies', 'Red Galaxies', 'Color Ratio'], [0.5, 0.95],
+                txtkw)
+    txtkw['ha'] = 'left'
+    txtkw['va'] = 'bottom'
+    fg.setColLabels(['Real Space', 'Redshift Space'], [0.05, 0.05], txtkw)
+    fg.makeYLabel(r'P$_{\rm{red}}$ (k) / P$_{\rm{blue}}$ (k)', 
+                [0, (0.5 * fg.panel_length + fg.yborder[0]) / fg.figsize[1]], 
+                {'va':'center', 'fontsize':larfont})
+    lkw = {'frameon':False, 'fontsize':smfont - 1, 'loc':'lower left'}
+    fg.drawLegend(lkw, (1,1))
+    lkw['loc'] = 'center right'
+    # fg.drawLegend(lkw, (2,0))
+    fcolors = np.empty(fg.dim, dtype = object)
+    trgba = mpl.colors.to_rgba
+    alpha = 0.15
+    for i in range(2):
+        flib.plotOnes(fg, (2, i))
+    # fcolors[:,0] = [trgba('blue', alpha), trgba('red', alpha), trgba('white')]
+    # flib.setFacecolor(fg, fcolors)
+    fg.save("redvsblue_redshift_evo.png")
